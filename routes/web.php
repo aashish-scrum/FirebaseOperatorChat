4602060266ce581b20aa80afe609e0ede6dae835
= <=>
2023-01-23 15:46:14 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
 
-Route::get('/chats', [App\Http\Controllers\HomeController::class, 'chat'])->name('chat');
-Route::get('/chat/visitors', [App\Http\Controllers\VisitorController::class, 'visitors'])->name('visitors');
-Route::get('/chat/operator/status/{operator_id}/{status}', [App\Http\Controllers\HomeController::class, 'operator_status'])->name('visitors');
-Route::post('/visitor/chat-end',[App\Http\Controllers\HomeController::class, 'chat_end']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::get('/chats', [App\Http\Controllers\HomeController::class, 'chat'])->name('chat');
+    Route::get('/chat/visitors', [App\Http\Controllers\VisitorController::class, 'visitors'])->name('visitors');
+    Route::get('/chat/operator/status/{operator_id}/{status}', [App\Http\Controllers\HomeController::class, 'operator_status'])->name('visitors');
+    Route::post('/visitor/chat-end',[App\Http\Controllers\HomeController::class, 'chat_end']);
+});
